28dadcb62c8d4e3e57ae28969b5774cc286fd904
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-12-01 20:13:32 -0800

    cleaned up main for debugging

--- main.cpp
+++ main.cpp
@@ -7,32 +7,33 @@ using namespace std;
 int main(int argc, char ** argv){
 
 
-  FileError* FE; /* used to check file input */
-  if(argc > 1){  /* if console argument provided */
-    FE = new FileError(argv[1]);
-
-    if(FE->pass() == false){
-      while(true){
-        FE->setFile(FE->getFile());
-        if(FE->pass()) break;
-      }
-    }
-  }
-  else { /* if no console argument provided - get file name */
-    FE = new FileError();
-
-    if(FE->pass() == false){
-      while(true){
-        FE->setFile(FE->getFile());
-        if(FE->pass()) break;
-      }
-    }
-  }
-  /* at this point, the user inputted file is valid */
-  string file = FE->getFileValue();
+  // FileError* FE; /* used to check file input */
+  // if(argc > 1){  /* if console argument provided */
+  //   FE = new FileError(argv[1]);
+  //
+  //   if(FE->pass() == false){
+  //     while(true){
+  //       FE->setFile(FE->getFile());
+  //       if(FE->pass()) break;
+  //     }
+  //   }
+  // }
+  // else { /* if no console argument provided - get file name */
+  //   FE = new FileError();
+  //
+  //   if(FE->pass() == false){
+  //     while(true){
+  //       FE->setFile(FE->getFile());
+  //       if(FE->pass()) break;
+  //     }
+  //   }
+  // }
+  // /* at this point, the user inputted file is valid */
+  // string file = FE->getFileValue();
 
   Database *db = new Database(file);
 
+<<<<<<< HEAD
   db->processFile();
 
   // db->addFaculty(22, "rene", "prof", "cs");
@@ -73,6 +74,31 @@ int main(int argc, char ** argv){
   // db->printStudents();
   // cout << "-------faculty-------" << endl;
   // db->printFaculty();
+=======
+  db->addFaculty(22, "rene", "prof", "cs");
+  db->addFaculty(3, "moshier", "prof", "math");
+  db->addFaculty(11, "harrison", "prof", "cs");
+  db->addFaculty(91, "stevens", "prof", "math");
+  //db->printFaculty();
+  db->deleteFaculty(11);
+  //db->printFaculty(); deleteFaculty works
+
+  db->addStudent(31, "krishna", "junior", "avfx/cs", 3.5, 22);
+  db->addStudent(43, "tarek", "junior", "cs", 3.5, 3);
+  db->addStudent(15, "zzzz", "junior", "avfx/cs", 3.5, 22);
+  //db->printStudents();
+  db->deleteStudent(31);
+  //db->printStudents(); deleteStudent works
+
+  //db->printFacultyAdvisees(22);
+  db->removeAdvisee(22, 31);
+  //db->printFacultyAdvisees(22); removeAdvisee works
+
+  cout << "-------students-------" << endl;
+  db->printStudents();
+  cout << "-------faculty-------" << endl;
+  db->printFaculty();
+>>>>>>> cleaned up main for debugging
   delete db;
 
   return 0;
